13978dc6bd575ca3cb02e4fbec220238514e16c9
oleksandrgrebinskij <roki000sasha@gmail.com>
2024-08-03 16:01:22 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): React.JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>HI</Text>
+    </SafeAreaView>
+  );
+}
+export default App;
